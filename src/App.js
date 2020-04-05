3a728e3e9036a17b011274fc1d6b1a12882d7289
Thaneshwor <thaneshwor100@gmail.com>
2020-04-06 01:49:14 +0545

    Include Landing component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import Navbar from './components/header/Navbar';
-import Footer from './components/footer/Footer';
+import Navbar from './components/layout/header/Navbar';
+import Footer from './components/layout/footer/Footer';
+import Landing from './components/home/Landing';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Landing />
         <Footer />
       </div >
     )
