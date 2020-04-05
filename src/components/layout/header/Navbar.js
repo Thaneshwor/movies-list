1576302f237b0c7fa9400f406d03f3df6a22764e
Thaneshwor <thaneshwor100@gmail.com>
2020-04-06 01:44:35 +0545

    Update images path

--- src/components/layout/header/Navbar.js
+++ src/components/layout/header/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './navbar.css';
-import HD from '../../images/HD.png';
-import Download from '../../images/download.jpg';
-import Hollywood from '../../images/hollywood.jpg';
-import Logo from '../../images/logo.jpg';
+import HD from '../../../images/HD.png';
+import Download from '../../../images/download.jpg';
+import Hollywood from '../../../images/hollywood.jpg';
+import Logo from '../../../images/logo.jpg';
 
 function Navbar() {
     return (
