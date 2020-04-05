256c30e00a2b05b714badad2d2a171cc6fe1341c
Thaneshwor <thaneshwor100@gmail.com>
2020-04-06 02:32:04 +0545

    User Provider component in App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import './App.css';
 import Navbar from './components/layout/header/Navbar';
 import Footer from './components/layout/footer/Footer';
 import Landing from './components/home/Landing';
+import { Provider } from 'react-redux';
 import store from './store';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Provider store={store}>
         <Navbar />
         <Landing />
         <Footer />
-      </div >
+      </Provider >
     )
   }
 }
