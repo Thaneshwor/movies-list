db98269f99cb372b6487132363f1e4ca1d02f47d
Thaneshwor <thaneshwor100@gmail.com>
2020-04-06 02:30:34 +0545

    Fix error

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
-import { crateStore, applyMiddleware, compose } from 'redux';
-import thunk from 'readux-thunk';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import rootReducer from './reducers'
 
 const middleware = [thunk];
 const initialState = {};
 
-const store = createStore(rootReducer, initialState, middleware, composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
